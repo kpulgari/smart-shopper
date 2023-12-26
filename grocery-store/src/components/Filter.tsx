74a4c2a02c09bfadf96aa2f670fef27153fb8660
Kaushik Pulgari <kpulg2@illinois.edu>
2023-12-26 12:50:43 -0600

    Change cart component to filter

---
+++ grocery-store/src/components/Filter.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Filter = () => {
+  return (
+    <div className="bg-cyan-100 rounded-lg ml-4 mb-4  overflow-x-hidden">
+      <div className="flex flex-col gap-4 m-4">
+        <div className="bg-white h-12 flex justify-center items-center w-full rounded-lg font-bold text-xl">
+          Filter
+        </div>
+        <div className="max-h-96 flex flex-row gap-2 overflow-y-auto px-2">
+          <div className="bg-white h-16 w-[50%] flex justify-left items-center rounded-lg p-4 font-bold m-0">
+            Row 1
+          </div>
+          <div className="bg-white h-16 w-[50%] flex justify-left items-center rounded-lg p-4 font-bold m-0">
+            Row 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
