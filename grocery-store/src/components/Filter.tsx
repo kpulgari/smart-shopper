f7d5fcbf2843aff764e8219c9b810b56f3e11bef
Kaushik Pulgari <kpulg2@illinois.edu>
2023-12-27 13:55:05 -0600

    Fix filter css

--- grocery-store/src/components/Filter.tsx
+++ grocery-store/src/components/Filter.tsx
@@ -8,6 +8,7 @@ export const Filter = () => {
   const [orderByPriceDesc, setOrderByPriceDesc] = useState(false);
 
   const handleOrderByAscClick = () => {
+    console.log(orderByAsc, orderByDesc, orderByPriceAsc, orderByPriceDesc);
     if (!orderByDesc) {
       setOrderByAsc(!orderByAsc);
     }
@@ -39,80 +40,88 @@ export const Filter = () => {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-2 overflow-hidden px-2 justify-center ">
           <div
-            className={`bg-white h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out cursor-pointer ${
-              orderByAsc
-                ? "bg-cyan-500 text-white"
-                : "hover:bg-cyan-300 hover:text-white"
+            className={`h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out ${
+              orderByAsc ? "bg-cyan-500 text-white cursor-pointer" : ""
             } ${
               orderByDesc ? "bg-slate-200 text-slate-400 cursor-default " : ""
+            } ${
+              !orderByAsc && !orderByDesc
+                ? "bg-white hover:bg-cyan-300 cursor-pointer"
+                : ""
             }`}
             onClick={handleOrderByAscClick}
           >
             <label
               htmlFor="orderByPriceDesc"
-              className={`m-2 cursor-pointer ${
-                orderByDesc ? "cursor-default " : ""
+              className={`m-2 ${
+                orderByDesc ? "cursor-default " : "cursor-pointer"
               }`}
             >
               A-Z
             </label>
           </div>
           <div
-            className={`bg-white h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out cursor-pointer ${
-              orderByDesc
-                ? "bg-cyan-500 text-white"
-                : "hover:bg-cyan-300 hover:text-white"
+            className={`h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out ${
+              orderByDesc ? "bg-cyan-500 text-white cursor-pointer" : ""
             } ${
               orderByAsc ? "bg-slate-200 text-slate-400 cursor-default " : ""
+            } ${
+              !orderByAsc && !orderByDesc
+                ? "bg-white hover:bg-cyan-300 cursor-pointer"
+                : ""
             }`}
             onClick={handleOrderByDescClick}
           >
             <label
               htmlFor="orderByPriceDesc"
-              className={`m-2 cursor-pointer ${
-                orderByAsc ? "cursor-default " : ""
+              className={`m-2 ${
+                orderByAsc ? "cursor-default " : "cursor-pointer"
               }`}
             >
               Z-A
             </label>
           </div>
           <div
-            className={`bg-white h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out cursor-pointer ${
-              orderByPriceAsc
-                ? "bg-cyan-500 text-white"
-                : "hover:bg-cyan-300 hover:text-white"
+            className={`h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out ${
+              orderByPriceAsc ? "bg-cyan-500 text-white cursor-pointer" : ""
             } ${
               orderByPriceDesc
                 ? "bg-slate-200 text-slate-400 cursor-default "
+                : ""
+            } ${
+              !orderByPriceAsc && !orderByPriceDesc
+                ? "bg-white hover:bg-cyan-300 cursor-pointer"
                 : ""
             }`}
             onClick={handleOrderByPriceAscClick}
           >
             <label
               htmlFor="orderByPriceDesc"
-              className={`m-2 cursor-pointer ${
-                orderByPriceDesc ? "cursor-default " : ""
+              className={`m-2 ${
+                orderByPriceDesc ? "cursor-default " : "cursor-pointer"
               }`}
             >
               Cheapest
             </label>
           </div>
           <div
-            className={`bg-white h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out cursor-pointer ${
-              orderByPriceDesc
-                ? "bg-cyan-500 text-white"
-                : "hover:bg-cyan-300 hover:text-white"
+            className={`h-16 w-full flex justify-center items-center rounded-lg p-4 font-bold m-0 transition duration-200 ease-in-out ${
+              orderByPriceDesc ? "bg-cyan-500 text-white cursor-pointer" : ""
             } ${
               orderByPriceAsc
                 ? "bg-slate-200 text-slate-400 cursor-default "
+                : ""
+            } ${
+              !orderByPriceDesc && !orderByPriceAsc
+                ? "bg-white hover:bg-cyan-300 cursor-pointer"
                 : ""
             }`}
             onClick={handleOrderByPriceDescClick}
           >
             <label
               htmlFor="orderByPriceDesc"
-              className={`m-2 cursor-pointer ${
-                orderByPriceAsc ? "cursor-default " : ""
+              className={`m-2 ${
+                orderByPriceAsc ? "cursor-default " : "cursor-pointer"
               }`}
             >
               Priciest
